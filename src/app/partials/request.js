d39aeb6248b5effe9c11e284c7e71766c43ce35e
Lemmon <lemmon@users.noreply.github.com>
2019-12-07 09:17:51 +0100

    request: send body

--- src/app/partials/request.js
+++ src/app/partials/request.js
@@ -10,7 +10,7 @@ const methods = [
 ]
 
 const handleChange = (e, app) => {
-  if (!app.state.request) app.state.request = {}
+  if (!app.state.request) app.state.request = { tab: 'body' }
   app.state.request[e.target.name] = e.target.value
   saveState(app.state)
 }
@@ -18,14 +18,16 @@ const handleChange = (e, app) => {
 const handleSubmit = (e, app) => {
   e.preventDefault()
   app.render()
+  const request = app.state.request
   const form = e.target
   const button = form.querySelector('button')
-  app.state.request.loading = true
   button.classList.add('button--loading')
   button.disabled = true
+  request.loading = true
   const t = Date.now()
-  fetch(form.elements['resource'].value, {
-    method: form.elements['method'].value,
+  fetch(request.resource, {
+    method: request.method,
+    body: request.body || undefined,
   }).then(async res => {
     const response = app.state.response = {
       ok: res.ok,
@@ -40,7 +42,7 @@ const handleSubmit = (e, app) => {
     if (response.type === 'application/json') {
       response.json = JSON.parse(response.body)
     }
-    delete app.state.request.loading
+    delete request.loading
     saveState(app.state)
     app.render()
   })
@@ -49,37 +51,47 @@ const handleSubmit = (e, app) => {
 module.exports = (props, app) => html`
   <div class="px1">
     <form
+      class="lh4"
       onchange=${e => handleChange(e, app)}
       onsubmit=${e => handleSubmit(e, app)}
     >
-      <div class="p05 row lh4">
-        <div class="p05 span1 row">
-          <label class="block py05 row"><select
-            class="input input--select py05"
-            name="method"
+      <div class="p05">
+        <div class="row">
+          <div class="p05 span1 row">
+            <label class="block py05 row"><select
+              class="input input--select py05"
+              name="method"
+            >
+              ${methods.map(curr => html`
+                <option selected=${curr === props.method}>${curr}</option>
+              `)}
+            </select><div class="py05 ml1">\u2193</div></label>
+            <div class="div px1 py025"></div>
+            <label class="block span1"><input
+              class="input py1"
+              type="url"
+              name="resource"
+              placeholder="http://"
+              required
+              value=${props.resource || ``}
+            /></label>
+          </div>
+          <div class="p05"><button
+            class="button ${props.loading ? `button--loading` : ``} bg-black color-white px2 py1"
+            type="submit"
+            disabled=${!!props.loading}
           >
-            ${methods.map(curr => html`
-              <option selected=${curr === props.method}>${curr}</option>
-            `)}
-          </select><div class="py05 ml1">\u2193</div></label>
-          <div class="div px1 py025"></div>
-          <label class="block span1"><input
-            class="input py1"
-            type="url"
-            name="resource"
-            placeholder="http://"
-            required
-            value=${props.resource || ``}
-          /></label>
+            <div class="button__caption">Send</div>
+            <div class="button__loader"></div>
+          </button></div>
         </div>
-        <div class="p05"><button
-          class="button ${props.loading ? `button--loading` : ``} bg-black color-white px2 py1"
-          type="submit"
-          disabled=${!!props.loading}
-        >
-          <div class="button__caption">Send</div>
-          <div class="button__loader"></div>
-        </button></div>
+        <div class="p05">
+          <label class="block ba"><textarea
+            class="input input--textarea p1b code f2"
+            style="height: 8rem; resize: none;"
+            name="body"
+          >${props.body || ``}</textarea></label>
+        </div>
       </div>
     </form>
   </div>
