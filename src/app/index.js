5d04602c6d340179bb83831718d29d656ab5cb2b
Lemmon <lemmon@users.noreply.github.com>
2020-01-07 15:05:38 +0100

    app default state

--- src/app/index.js
+++ src/app/index.js
@@ -3,7 +3,10 @@ const rege = require('./rege')
 const page = require('./pages/default')
 
 const init = JSON.parse(window.localStorage.getItem('postperson'))
+const defaultState = {
+  request: { tab: 0 },
+}
 
 require('@lemmon/custom-textarea/element')
 
-rege(app => page(app), init || {}).render()
+rege(app => page(app), init || defaultState).render()
--- src/app/pages/default.js
+++ src/app/pages/default.js
@@ -5,7 +5,7 @@ const renderResponse = require('../partials/response')
 module.exports = (app) => html`
   <div id="app" class="span1 col">
 
-    ${renderRequest(app.state.request || {}, app)}
+    ${renderRequest(app.state.request, app)}
     <hr class="div px1" style="--border-width: 2px;"/>
     ${renderResponse(app.state.response, app)}
 
